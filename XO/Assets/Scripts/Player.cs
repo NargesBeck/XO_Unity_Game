45817a1061ca44fbb35486a86198e9968ef8bc68
NargesBeck <60057086+NargesBeck@users.noreply.github.com>
2022-06-28 18:41:08 +0430

    UI (All 3 Panels), Player Class

---
+++ XO/Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+public enum XO
+{
+    X, O
+}
+
+public class Player : MonoBehaviour
+{
+    public XO currentXO;
+    private int numOfWins;
+
+    public void PlayerWon()
+    {
+        numOfWins++;
+    }
+}
